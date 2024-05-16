a315829e59669431fae38cf288a5b90db841eb4b
Amanda Furtado <amandaflsr.estudos@gmail.com>
2024-05-16 08:22:29 -0300

    next level working

--- Game/Worlds/Scripts/TheMoonLevel.gd
+++ Game/Worlds/Scripts/TheMoonLevel.gd
@@ -1,13 +1,35 @@
-extends Level
+extends Node2D
 
-
+@export var n_lvl: PackedScene
+@onready var player = $Player
+#boss related
+@onready var boss = $TheMoon
 @onready var moon_sea = $MoonSea
+#Interface
+@onready var label = $CanvasLayer/Label
+@onready var hud = $CanvasLayer/Hud
 
 
 func _ready() -> void:
-	super()
+	#Interface
+	player.stats.health_changed.connect(func():
+		hud.update_health_bar(player.stats.health))
 	
+	boss.stats.health_changed.connect(func():
+		hud.update_boss_health_bar(boss.stats.health))
+	
+	#Scene Change
+	boss.stats.no_health.connect(func():
+		await get_tree().create_timer(2.0).timeout
+		get_tree().change_scene_to_packed(n_lvl)
+		)
+	
+	#boss related
 	boss.touch_top.connect(func():
 		moon_sea.pull_top_ocean())
 	boss.touch_bot.connect(func():
 		moon_sea.pull_bot_ocean())
+
+
+func _process(_delta: float) -> void:
+	label.text = str("FPS: ", Engine.get_frames_per_second())   
--- Game/Worlds/Scripts/TheStarLevel.gd
+++ Game/Worlds/Scripts/TheStarLevel.gd
@@ -1,10 +1,31 @@
-extends Level
+extends Node2D
 
 
+@onready var player = $Player
+#boss related
+@onready var boss = $TheStar
+#Interface
+@onready var label = $CanvasLayer/Label
+@onready var hud = $CanvasLayer/Hud
 
 func _ready():
-	super()
+	player.stats.health_changed.connect(func():
+		hud.update_health_bar(player.stats.health))
+	
+	boss.stats.health_changed.connect(func():
+		hud.update_boss_health_bar(boss.stats.health))
+	
+	boss.stats.no_health.connect(func():
+		pass
+		)
 
 
-func _process(delta):
-	super(delta)
+func _process(_delta: float) -> void:
+	label.text = str("FPS: ", Engine.get_frames_per_second())   
+
+
+func go_to_next_level(level: PackedScene):
+	if not level is PackedScene:
+		print("NO LEVEL SELECTED")
+		return
+	get_tree().change_scene_to_packed(level)
