8af7af05b261c171a607426fab9aac8250ad21c3
Amanda Furtado <amandaflsr.estudos@gmail.com>
2024-05-18 19:39:58 -0300

    lots to do, gonna go over night yey

--- Game/Bosses/The Moon/Scripts/MoonSea.gd
+++ Game/Bosses/The Moon/Scripts/MoonSea.gd
@@ -5,6 +5,7 @@ extends Node2D
 @onready var bot_ocean = $bot_ocean
 @onready var top_spawner = $top_ocean/TopSpawner
 @onready var bot_spawner = $bot_ocean/BotSpawner
+@onready var audio = $Audio
 
 
 func ocean_phase2():
@@ -15,13 +16,19 @@ func ocean_phase2():
 
 func pull_top_ocean():
 	ocean_up(top_ocean, top_spawner)
+	audio.pitch_scale = randf_range(0.9, 1.1)
+	audio.play()
 	await get_tree().create_timer(2.0).timeout
+	
 	ocean_down(top_ocean)
 
 
 func pull_bot_ocean():
 	ocean_up(bot_ocean, bot_spawner)
+	audio.pitch_scale = randf_range(0.9, 1.1)
+	audio.play()
 	await get_tree().create_timer(2.0).timeout
+	
 	ocean_down(bot_ocean)
 
 
