c3928af1947e5033d86863bca60dde832eb35380
Amanda Furtado <amandaflsr.estudos@gmail.com>
2024-05-20 10:10:52 -0300

    more menus

--- Game/User Interface/Scripts/DebugMenu.gd
+++ Game/User Interface/Scripts/DebugMenu.gd
@@ -6,19 +6,37 @@ extends Control
 @onready var sun_scene: PackedScene = load("res://Game/Worlds/TheSunLevel.tscn")
 
 @onready var moon_button = $VBoxContainer/MoonButton
+@onready var star_button = $VBoxContainer/StarButton
+@onready var sun_button = $VBoxContainer/SunButton
 
+@onready var button_audio = $ButtonAudio
 
 func _ready() -> void:
 	moon_button.grab_focus()
 
 
 func _on_moon_button_pressed():
+	button_audio.play()
+	
+	moon_button.disabled = true
+	star_button.disabled = true
+	sun_button.disabled = true
 	SceneManager.load_new_scene(moon_scene.resource_path)
 
 
 func _on_star_button_pressed():
+	button_audio.play()
+	
+	moon_button.disabled = true
+	star_button.disabled = true
+	sun_button.disabled = true
 	SceneManager.load_new_scene(star_scene.resource_path)
 
 
 func _on_sun_button_pressed():
+	button_audio.play()
+	
+	moon_button.disabled = true
+	star_button.disabled = true
+	sun_button.disabled = true
 	SceneManager.load_new_scene(sun_scene.resource_path)
--- Game/User Interface/Scripts/StartMenu.gd
+++ Game/User Interface/Scripts/StartMenu.gd
@@ -9,6 +9,7 @@ extends Control
 @onready var config_button = %ConfigButton
 
 @onready var button_audio = $ButtonAudio
+@onready var selection_button = %SelectionButton
 
 
 func _ready() -> void:
@@ -19,6 +20,7 @@ func _on_start_button_pressed() -> void:
 	button_audio.play()
 	start_button.disabled = true
 	config_button.disabled = true
+	selection_button.disabled = true
 	SceneManager.load_new_scene(first_level.resource_path)
 
 
@@ -26,8 +28,17 @@ func _on_config_button_pressed():
 	button_audio.play()
 	start_button.disabled = true
 	config_button.disabled = true
+	selection_button.disabled = true
 	SceneManager.load_new_scene(settings_scene.resource_path)
 
 
 #func _on_exit_button_pressed() -> void:
 	#get_tree().quit()
+
+
+func _on_selection_button_pressed():
+	button_audio.play()
+	start_button.disabled = true
+	config_button.disabled = true
+	selection_button.disabled = true
+	SceneManager.load_new_scene("res://Game/User Interface/DebugMenu.tscn")
